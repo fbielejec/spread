8f03ec19ca77af768cd4fb8671f32b971bde16cb
Juan Monetta <jpmonettas@gmail.com>
2021-09-02 11:08:25 -0300

    Fixed analysis result -> data tabs for 3 types

--- src/cljs/ui/component/app_container.cljs
+++ src/cljs/ui/component/app_container.cljs
@@ -134,8 +134,6 @@
     [collapsible-tab {:id :run-new
                       :title "Run new analysis"
                       :icon "icons/icn_run_analysis.svg"
-                      :badge-text "2 new"
-                      :badge-color "purple"
                       :child [:div.run-new
                               (map-indexed (fn [index {:keys [main-label sub-label target query]}]
                                              [:li.clickable {:key      index
